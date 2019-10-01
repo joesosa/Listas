93ad2a9e24c7739a2c0433e1d538f6acb71cdf6a
Danhgz <43118660+Danhgz@users.noreply.github.com>
2019-09-30 19:18:47 -0600

    Listo el LP_Arreglo

--- ListaPosicionada_Arreglo.cpp
+++ ListaPosicionada_Arreglo.cpp
@@ -1,31 +1,69 @@
-// 1. Implementar el modelo Lista Posicionada utilizando las estructuras de datos: Arreglo.
+#include "ListaPosicionada_Arreglo.h"
 
-#ifndef _LISTAP
-#define _LISTAP
-#include<iostream>
-using namespace std;
+ListaPos::ListaPos(int tamanoMaximo){ //Iniciar(L)
+	vListaPos = new int[tamanoMaximo];
+	ultimoLleno = -1;
+}	
 
-class ListaPos {
-    class Celda;    // Prototipo
-    private:
-		int* listaPos; 
-		int ultimoLleno;
-		
-	public:
-		ListaPos(int); //Iniciar(L)
-		~ListaPos(); //Destruir(L)
-		void vaciar(); //Vaciar(L)
-		bool vacia(); //Vacia(L)
-		void insertar(int,int); //Insertar(e,p,L)
-		void agregarUltimo(int); //AgregarUltimo(e,L)
-		void borrar(int); //Borrar(p,L)
-		int recuperar(int); //Recuperar(p,L)
-		void modificar(int, int); //Modificar(p,e,L)
-		void intercambiar(int,int); //Intercambiar(p1,p2,L)
-		int numElem(); //NumElem(L)
-		int primera(); //Primera(L)
-		int ultima(); //Ultima(L)
-		int siguiente(int); //Siguiente(L)
-		int anterior(int); //Anterior(L)
-};
-#endif
+ListaPos::~ListaPos(){
+	delete vListaPos;	//Destruir(L)
+}
+
+void ListaPos::vaciar(){ //Vaciar(L)
+	ultimoLleno = -1;
+}
+
+bool ListaPos::vacia(){ //Vacia(L)
+	return (ultimoLleno == -1); 
+}
+
+void ListaPos::insertar(int elem, int pos){ //Insertar(e,p,L)
+	for(int i = ultimoLleno+1; i> pos ;--i)
+		vListaPos[i]= vListaPos[i-1];
+	vListaPos[pos]= elem;
+	++ultimoLleno;
+}
+
+void ListaPos::agregarUltimo(int elem){ //AgregarUltimo(e,L)
+	vListaPos[++ultimoLleno] = elem;
+}
+
+void ListaPos::borrar(int pos){ //Borrar(p,L)
+	for(int i = pos; i< ultimoLleno ;++i)
+			vListaPos[i]= vListaPos[i+1];
+	--ultimoLleno;
+}
+
+int ListaPos::recuperar(int pos){ //Recuperar(p,L)
+	return vListaPos[pos];
+}
+
+void ListaPos::modificar(int pos, int elem){ //Modificar(p,e,L)
+	vListaPos[pos] = elem;
+}
+
+void ListaPos::intercambiar(int pos1, int pos2){ //Intercambiar(p1,p2,L)
+	int auxiliar = vListaPos[pos1];
+	vListaPos[pos1] = vListaPos[pos2];
+	vListaPos[pos2] = auxiliar;
+}
+
+int ListaPos::numElem(){ //NumElem(L)
+	return ultimoLleno+1;
+}
+
+int ListaPos::primera(){ //Primera(L)
+	return vListaPos[0];
+}
+
+int ListaPos::ultima(){ //Ultima(L)
+	return vListaPos[ultimoLleno];
+}
+
+int ListaPos::siguiente(int pos){ //Siguiente(p,L)
+	return (pos!=ultimoLleno)? pos+1 : -1;
+}
+
+int ListaPos::anterior(int pos){ //Anterior(p,L)
+	return pos-1;
+}
---
+++ ListaPosicionada_Arreglo.h
@@ -0,0 +1,31 @@
+// 1. Implementar el modelo Lista Posicionada utilizando las estructuras de datos: Arreglo.
+
+#ifndef _LISTAP
+#define _LISTAP
+#include<iostream>
+using namespace std;
+
+class ListaPos {
+    class Celda;    // Prototipo
+    private:
+		int* vListaPos; 
+		int ultimoLleno;
+		
+	public: //pene
+		ListaPos(int); //Iniciar(L)
+		~ListaPos(); //Destruir(L)
+		void vaciar(); //Vaciar(L)
+		bool vacia(); //Vacia(L)
+		void insertar(int,int); //Insertar(e,p,L)
+		void agregarUltimo(int); //AgregarUltimo(e,L)
+		void borrar(int); //Borrar(p,L)
+		int recuperar(int); //Recuperar(p,L)
+		void modificar(int, int); //Modificar(p,e,L)
+		void intercambiar(int,int); //Intercambiar(p1,p2,L)
+		int numElem(); //NumElem(L)
+		int primera(); //Primera(L)
+		int ultima(); //Ultima(L)
+		int siguiente(int); //Siguiente(L)
+		int anterior(int); //Anterior(L)
+};
+#endif
