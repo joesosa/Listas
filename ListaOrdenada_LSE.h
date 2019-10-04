bf24854389ffea150322d8ba52f12b43d71a4489
Danhgz <43118660+Danhgz@users.noreply.github.com>
2019-10-04 16:07:40 -0600

    Update ListaOrdenada_LSE.h

--- ListaOrdenada_LSE.h
+++ ListaOrdenada_LSE.h
@@ -6,27 +6,28 @@ using namespace std;
 class ListaOrdenada {
     class Celda;
     private:
-    Celda * primera;
-		Celda * ultima;
-    int numElementos;
-     class Celda{
-         public:
-               int elemento;
-           Celda * siguiente;
-           Celda(int);
-           ~Celda();
-      };
+    	Celda * primera;
+	Celda * ultima;
+    	int numElementos;
+     	class Celda{
+        	public:
+               		int elemento;
+           		Celda * siguiente;
+           		Celda(int);
+           		~Celda();
+      	};
 	public:
 		ListaOrdenada(); //Iniciar(L)
 		~ListaOrdenada(); //Destruir(L)
 		void vaciar(); //Vaciar(L)
 		bool vacia(); //Vacia(L)
 		void insertar(int); //Insertar(e,L)
-    void borrar(int); //Borrar(e,L)
-    int primero(); // Primero(L)
-    int ultimo(); // Ultimo(L)
-    int siguiente(int); // Siguiente(e,L)
-    int anterior(int); // Anterior(e,L)
+    		void borrar(int); //Borrar(e,L)
+    		int primero(); // Primero(L)
+    		int ultimo(); // Ultimo(L)
+    		int siguiente(int); // Siguiente(e,L)
+    		int anterior(int); // Anterior(e,L)
 		int numElem(); //NumElem(L)
+		char* getId();
 };
 #endif
