a693c0d7985536ec02935700a98ecdb0f4790d8e
joesosa <44095239+joesosa@users.noreply.github.com>
2019-10-15 00:17:28 -0600

    Add files via upload

--- Algoritmos.cpp
+++ Algoritmos.cpp
@@ -83,7 +83,7 @@ void unionListas(ListaOrdenada& lista1, ListaOrdenada& lista2, ListaOrdenada& li
 bool buscar(ListaPos& lista, int elemento){
 	bool existe = false;
 	ListaPos::posicion pos = lista.getPrimera();
-	while (!existe && pos){
+	while (!existe && pos){ //pos?
 		if( lista.recuperar(pos) == elemento ){
 			existe = true;
 		}
@@ -94,7 +94,7 @@ bool buscar(ListaPos& lista, int elemento){
 
 bool buscar(ListaInd& lista, int elemento){
 	bool existe = false;
-	int actual = lista.recuperar(0);
+	int actual = 0;
 	while (!existe && actual < lista.numElem()){
 		if( lista.recuperar(actual) == elemento ){
 			existe = true;
@@ -148,50 +148,122 @@ bool simetrica(ListaPos& lista){
 		posI = lista.siguiente(posI);
 		posF = lista.anterior(posF);
 	}
+	if (lista.recuperar(posI) != lista.recuperar(posF)) {
+		esSimetrica = false;
+	}
 	return esSimetrica;
 }
 
-bool simetrica(ListaInd& lista){
+bool simetrica(ListaInd& lista) {
 	int indI = 0;
-	int indF = lista.numElem()-1;
+	int indF = lista.numElem() - 1;
 	bool esSimetrica = true;
-	while(esSimetrica && indI != indF && indI+1 != indF){
-		if(indI != indF){
+	while (esSimetrica && indI != indF && indI + 1 != indF) {
+		if (lista.recuperar(indI) != lista.recuperar(indF)) {
 			esSimetrica = false;
 		}
 		++indI;
 		--indF;
 	}
+	if (lista.recuperar(indI) != lista.recuperar(indF)) {
+		esSimetrica = false;
+	}
 	return esSimetrica;
 }
 
 /*
-@Efecto: Ordena los elementos de la lista en orden ascendente
+@Efecto: Ordena los elementos de la lista en orden ascendente y se manera secuencial iterativa mediante...
 @Requiere: Una lista no vacia y de tamano minimo de dos
 @Modifica: Este metodo modifica la lista
 */
 void seleccionIterativo(ListaPos& lista){
 	ListaPos::posicion posMin;																																												
-	for(ListaPos::posicion i = lista.getPrimera(); i != lista.getUltima(); i = lista.siguiente(i)){
+	for(ListaPos::posicion i = lista.getPrimera(); i != lista.anterior(lista.getUltima()) ; i = lista.siguiente(i)){
 		posMin = i;
 		for(ListaPos::posicion j = lista.siguiente(i); j != lista.getUltima(); j = lista.siguiente(j){
-			if(lista.recuperar(j) < lista.recuperar(minimo)){
-				min = j;
+			if(lista.recuperar(j) < lista.recuperar(posMin)){
+				posMin = j;
 			}
 		}
 		lista.intercambiar(i,minimo);
 	}
 }
 
-void seleccionIterativo(ListaInd& lista){
-	int minimo = 0;																																													
-	for(int i = 0; i < lista.numElem() - 1; ++i){
-		minimo = i;
-		for(int j = i+1; j < lista.numElem(); ++j){
-			if(lista.recuperar(j) < lista.recuperar(minimo)){
-				minimo = j;
+void seleccionIterativo(ListaInd& lista) {
+	int minimo = 0;
+		for (int i = 0; i < lista.numElem() - 1; ++i) {
+			minimo = i;
+			for (int j = i + 1; j < lista.numElem(); ++j) {
+				if (lista.recuperar(j) < lista.recuperar(minimo)) {
+					minimo = j;
+				}
+			}
+			lista.intercambiar(i, minimo);
+		}
+}
+
+/*
+@Efecto: 
+@Requiere: Una lista no vacia y de tamano minimo de dos
+@Modifica: Este metodo modifica la lista
+*/
+//posible error
+void seleccionRecursivo(ListaPos& lista, int inicio) {
+	ListaPos::posicion i = traducir(lista, inicio);
+	if (i == lista.getUltima()) {
+		return;
+	}
+	else {
+		ListaPos::posicion minimo = i;
+		for (i = lista.siguiente(lista.getPrimera()); i != lista.siguiente(lista.getUltima()); i = lista.siguiente(i)) {
+			if (lista.recuperar(i) < lista.recuperar(minimo)) {
+				minino = i
 			}
 		}
-		lista.intercambiar(i,minimo);
+		lista.intercambiar(inicio, minimo);
+		seleccionRecursivo(lista, ++inicio);
 	}
 }
+
+void seleccionRecursivo(ListaInd& lista, int inicio) {
+	if (inicio == lista.numElem() - 1) {
+		return;
+	}
+	else {
+		int minimo = inicio;
+		for (int i = inicio + 1; i < lista.numElem(); ++i) {
+			if (lista.recuperar(i) < lista.recuperar(minimo)) {
+				minimo = i;
+			}
+		}
+		lista.intercambiar(inicio, minimo);
+		seleccionRecursivo(lista, ++inicio);
+	}
+}
+
+/*
+@Efecto: 
+@Requiere: Una lista no vacia y de tamano minimo de dos
+@Modifica: Este metodo modifica la lista
+*/
+//posible error
+void burbujaOriginal(ListaPos& lista) { //revisar
+	for (ListaPos::posicion i = lista.getPrimera(); i != lista.anterior(lista.getUltima()); i = lista.siguiente(i)) {
+		for (ListaPos::posicion j = lista.getPrimera(); ; j = lista.siguiente(j)) { //solucionar error
+			if (lista.recuperar(j) < lista.recuperar(lista.siguiente(j))) {
+				lista.intercambiar(lista.siguiente(j) , j);
+			}
+		}
+	}
+}
+
+void burbujaOriginal(ListaInd& lista) {
+	for (int i = 0; i < lista.numElem() - 1; ++i) {
+		for (int j = 0; j < lista.numElem() - i - 1; ++j) {
+			if (lista.recuperar(j) > lista.recuperar(j + 1)) {
+				lista.intercambiar(j + 1, j);
+			}
+		}
+	}
+}
+
